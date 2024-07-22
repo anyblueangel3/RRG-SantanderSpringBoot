ae848809d9c64e7f912b7fc91552d203ce25dd43
Ronaldo Rodrigues Godoi <anyblueangel3@gmail.com>
2024-07-22 09:05:26 -0300

    O projeto recebeu o projeto do Spring Boot.

---
+++ demo1/src/test/java/RRG_SantanderSpringBoot/demo3/Demo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package RRG_SantanderSpringBoot.demo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
